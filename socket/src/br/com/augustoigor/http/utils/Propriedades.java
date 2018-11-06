ba1e2abf42680e8f4aa4886ef81c0edf7b43872c
igoroliv <igoroliv@NTB127951.corp.capgemini.com>
2018-11-06 17:39:35 -0200

    Adicionando properties

---
+++ socket/src/br/com/augustoigor/http/utils/Propriedades.java
@@ -0,0 +1,11 @@
+package br.com.augustoigor.http.utils;
+
+import java.util.ResourceBundle;
+
+public class Propriedades {
+
+	static ResourceBundle rb = ResourceBundle.getBundle("propriedades");
+	
+	public static String diretorio = rb.getString("diretorio");
+	
+}
